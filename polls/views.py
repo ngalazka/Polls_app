e068efc83c128b9065f3709653f6c3bebe428e5e
ngalazka <nat.galazka@gmail.com>
2018-11-18 12:54:27 +0100

    Zarejestrowanie modeli w adminie, dodanie widoków

--- polls/views.py
+++ polls/views.py
@@ -8,3 +8,13 @@ def index(request):
     questions = Question.objects.all()
     questions = [(question.id, question.pub_date, question.question_text) for question in questions]
     return HttpResponse(questions)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
